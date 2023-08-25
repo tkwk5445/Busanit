4471259b5ddbaa62b1165d58f2b9ae7a2347dedf
Wookjin <131837195+tkwk5445@users.noreply.github.com>
2023-08-25 16:07:10 +0900

    Add files via upload

---
+++ k8s/Initial Installation/install_pkg.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# install packages 
+yum install epel-release -y
+yum install vim-enhanced -y
+yum install git -y
+
+# install docker 
+yum install docker -y && systemctl enable --now docker
+
+# install kubernetes cluster 
+yum install kubectl-$1 kubelet-$1 kubeadm-$1 -y
+systemctl enable --now kubelet
